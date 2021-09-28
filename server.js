64c3ec566722ccf73c6de30189a963596664d827
helloitsjoe <jboyle617@gmail.com>
2021-09-28 08:40:30 -0400

    add custom server

---
+++ server.js
@@ -0,0 +1,24 @@
+// Custom server
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    const { pathname, query } = parsedUrl;
+
+    if (pathname === '/custom') {
+      app.render(req, res, '/custom', query);
+    } else {
+      handle(req, res, parsedUrl);
+    }
+  }).listen(3000, err => {
+    if (err) throw err;
+    console.log('Listening on http://localhost:3000');
+  });
+});
