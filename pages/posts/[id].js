96414e66d6a2b492d652b40a30753c67d229de88
helloitsjoe <jboyle617@gmail.com>
2021-10-07 22:26:34 -0400

    getServerSideProps

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,9 +2,9 @@ import Head from 'next/head';
 import Date from '../../components/Date';
 import Layout from '../../components/Layout';
 import utilStyles from '../../styles/utils.module.css';
-import { getAllPostIds, getPostData } from '../../lib/posts';
+import { getPostData } from '../../lib/posts';
 
-const Post = ({ title, id, date, contentHtml }) => {
+const Post = ({ title, id, date, contentHtml, random }) => {
   return (
     <Layout>
       <Head>
@@ -15,18 +15,26 @@ const Post = ({ title, id, date, contentHtml }) => {
         <div className={utilStyles.lightText}>
           <Date dateString={date} />
         </div>
+        <div className={utilStyles.lightText}>Random number: {random}</div>
         <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
       </article>
     </Layout>
   );
 };
 
-export async function getStaticPaths() {
-  return { paths: getAllPostIds(), fallback: false };
-}
+// export async function getStaticPaths() {
+//   return { paths: getAllPostIds(), fallback: false };
+// }
 
-export async function getStaticProps({ params }) {
-  return { props: await getPostData(params.id) };
+// export async function getStaticProps({ params }) {
+//   return { props: await getPostData(params.id) };
+// }
+
+export async function getServerSideProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: { ...postData, random: Math.floor(Math.random() * 100) },
+  };
 }
 
 export default Post;
